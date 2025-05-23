902d2c621e7ec90eafe1f46214fe96e2483b81c3
zirizenati <zirizenati@gmail.com>
2025-05-23 06:02:14 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thank you for your submission! We will contact you soon.');
-  this.reset();
-});
